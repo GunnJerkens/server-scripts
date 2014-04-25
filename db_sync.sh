43c690e0e52995b990d75e15680317c446b8c51d
Christopher Lovejoy <clovejoy@gunnjerkens.com>
2014-04-24 17:03:11 -0700

    put password in double quote, to allow substitution

--- db_sync.sh
+++ db_sync.sh
@@ -5,10 +5,11 @@
 if [ -z "$remote_env" ]; then
   echo "this server appears to be at the top of the food chain (production?)"
   exit 1
-elif [ -z "$remote_ssh" ]; then
-  echo "please specify a remote SSH user"
-  exit 1
 fi
 
 echo -n "(local mysql) "
-ssh -p $remote_ssh_port $remote_ssh \ "mysqldump -u$remote_username -p'$remote_password' $remote_database" \ | mysql -u$env_username -p$env_password $env_database
+if [ -z "$remote_ssh" ]; then
+  mysqldump -u$remote_username -p"$remote_password" $remote_database | mysql -u$env_username -p$env_password $env_database
+else
+  ssh -p $remote_ssh_port $remote_ssh \ "mysqldump -u$remote_username -p\"$remote_password\" $remote_database" \ | mysql -u$env_username -p$env_password $env_database
+fi
